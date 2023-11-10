9c1a8df732520e7484fe44c63e2de86c53b4198d
haldola05 <146481065+haldola05@users.noreply.github.com>
2023-11-10 12:36:13 +0100

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,14 +1,26 @@
-
-
-if __name__ == "__main__":
 def generate_random_data():
 
-def main():
-    return data
-
 import random
+
+if __name__ == "__main__":
+    for item in data:
+        print(f"Random Number: {item}")
+
     main()
+    return data
+    data = generate_random_data()
     data = [random.randint(1, 100) for _ in range(10)]
+
+def main():    return data
+if __name__ == "__main__":
+    for item in data:
         print(f"Random Number: {item}")
     data = generate_random_data()
-    for item in data:
+def main():
+    main()
+
+
+def generate_random_data():
+
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
